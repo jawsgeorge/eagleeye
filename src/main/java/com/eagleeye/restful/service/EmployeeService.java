ab6def3fde4c31ea2bc487731dc7df39dfe8199a
gjebaraj <gjebaraj@GJEBARAJ-DGH6B.partnet.cisco.com>
2017-04-12 10:41:53 +0530

    Test changes

---
+++ src/main/java/com/eagleeye/restful/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.eagleeye.restful.service;
+
+import com.eagleeye.restful.model.Employee;
+
+/**
+ * 
+ * @author BytesTree
+ *
+ */
+public interface EmployeeService extends CRUDService<Employee> {
+
+}
