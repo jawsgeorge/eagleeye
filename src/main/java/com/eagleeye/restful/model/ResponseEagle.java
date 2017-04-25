1dc388ae298ee3bb407c4577349c7e8e49efdedc
gjebaraj <gjebaraj@GJEBARAJ-DGH6B.partnet.cisco.com>
2017-04-25 15:52:52 +0530

    Response code class

---
+++ src/main/java/com/eagleeye/restful/model/ResponseEagle.java
@@ -0,0 +1,20 @@
+package com.eagleeye.restful.model;
+
+public class ResponseEagle {
+private String responseCode;
+public String getResponseCode() {
+	return responseCode;
+}
+public void setResponseCode(String responseCode) {
+	this.responseCode = responseCode;
+}
+public String getResponseMessage() {
+	return responseMessage;
+}
+public void setResponseMessage(String responseMessage) {
+	this.responseMessage = responseMessage;
+}
+
+private String responseMessage;
+
+}
