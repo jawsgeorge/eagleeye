36847925396fbbd30607682c2e65f2847a6bb86a
rmuruga2 <rmuruga2@RMURUGA2-31TCB.partnet.cisco.com>
2017-04-27 14:33:24 +0530

    Ground related methods in controller

--- src/main/java/com/eagleeye/restful/controller/EagleEyeController.java
+++ src/main/java/com/eagleeye/restful/controller/EagleEyeController.java
@@ -2,7 +2,11 @@ package com.eagleeye.restful.controller;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -17,13 +21,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eagleeye.restful.model.Employee;
+import com.eagleeye.restful.model.Ground;
+import com.eagleeye.restful.model.GroundDao;
 import com.eagleeye.restful.model.Menu;
 import com.eagleeye.restful.model.ResponseEagle;
 import com.eagleeye.restful.model.Role;
 import com.eagleeye.restful.service.AddMenu;
 import com.eagleeye.restful.service.AddRole;
 import com.eagleeye.restful.service.EmployeeService;
-
+import com.eagleeye.restful.service.GroundService;
 import com.eagleeye.restful.model.User;
 import com.eagleeye.restful.service.UserService;
 import com.eagleeye.restful.model.UserDAO;
@@ -44,13 +50,16 @@ public class EagleEyeController {
 	EmployeeService empService;
 	
 	 @Autowired
-	 private AddMenu menuservice;
+	 private AddMenu menuService;
 	  
 	 @Autowired
-	 private AddRole roleservice;
+	 private AddRole roleService;
 	
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	GroundService groundService;
 	
 	// Private fields
 	  
@@ -58,17 +67,19 @@ public class EagleEyeController {
 	  @Autowired
 	  private UserDAO userDao;
 	  
-	 
+	 @Autowired
+	 GroundDao groundDao;
 	
 	//User Controller
 
 	@RequestMapping(value="/adduser",method = RequestMethod.POST)
-	public ResponseEntity<User> addUser(@RequestBody User user) {
+	public ResponseEntity<List<User>> addUser(@RequestBody User user) {
 		//To do change for hardcode value
 		user.setPassword("eagleeye");
 		userService.save(user);
 		logger.debug("Added User sucessfully:: " + user);
-		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+		List<User> userList = userService.getAll();
+		return new ResponseEntity<List<User>>(userList, HttpStatus.CREATED);
 	}
 
 
@@ -115,18 +126,18 @@ public class EagleEyeController {
 	
 	@RequestMapping(value="/addMenu",method = RequestMethod.POST)
 	public ResponseEntity<Menu> addMenu(@RequestBody Menu menu) {
-		menuservice.save(menu);
+		menuService.save(menu);
 		logger.debug("Added:: " + menu);
 		return new ResponseEntity<Menu>(menu, HttpStatus.CREATED);
 	}
 	@RequestMapping(value="/updateMenu",method = RequestMethod.PUT)
 	public ResponseEntity<Void> updateMenu(@RequestBody Menu menu) {
-		Menu existingMenu = menuservice.getById(menu.getMenu_id());
+		Menu existingMenu = menuService.getById(menu.getMenu_id());
 		if (existingMenu == null) {
 			logger.debug("Menu with id " + menu.getMenu_id() + " does not exists");
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 		} else {
-			menuservice.save(menu);
+			menuService.save(menu);
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
 	}
@@ -134,7 +145,7 @@ public class EagleEyeController {
 
 	@RequestMapping(value = "getMenu/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Menu> getMenu(@PathVariable("id") Long id) {
-		Menu menu = menuservice.getById(id);
+		Menu menu = menuService.getById(id);
 		if (menu == null) {
 			logger.debug("Menu with id " + id + "  does not exists");
 			return new ResponseEntity<Menu>(HttpStatus.NOT_FOUND);
@@ -147,7 +158,7 @@ public class EagleEyeController {
 
 	@RequestMapping(value="/getMenu",method = RequestMethod.GET)
 	public ResponseEntity<List<Menu>> getAllMenu() {
-		List<Menu> menuList = menuservice.getAll();
+		List<Menu> menuList = menuService.getAll();
 		if (menuList.isEmpty()) {
 			logger.debug("Menu does not exists");
 			return new ResponseEntity<List<Menu>>(HttpStatus.NO_CONTENT);
@@ -160,12 +171,12 @@ public class EagleEyeController {
 
 	@RequestMapping(value = "deleteMenu/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deleteMenu(@PathVariable("id") Long id) {
-		Menu menu = menuservice.getById(id);
+		Menu menu = menuService.getById(id);
 		if (menu == null) {
 			logger.debug("Menu with id " + id + " does not exists");
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 		} else {
-			menuservice.delete(id);
+			menuService.delete(id);
 			logger.debug("Menu with id " + id + " deleted");
 			return new ResponseEntity<Void>(HttpStatus.GONE);
 		}
@@ -174,18 +185,18 @@ public class EagleEyeController {
 	// Role Controller
 	@RequestMapping(value="/addRole",method = RequestMethod.POST)
 	public ResponseEntity<Role> addRole(@RequestBody Role role) {
-		roleservice.save(role);
+		roleService.save(role);
 		logger.debug("Added:: " + role);
 		return new ResponseEntity<Role>(role, HttpStatus.CREATED);
 	}
 	@RequestMapping(value="/updateRole",method = RequestMethod.PUT)
 	public ResponseEntity<Void> updateRole(@RequestBody Role role) {
-		Role existingRole = roleservice.getById(role.getRole_id());
+		Role existingRole = roleService.getById(role.getRole_id());
 		if (existingRole == null) {
 			logger.debug("Role with id " + role.getRole_id() + " does not exists");
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 		} else {
-			roleservice.save(role);
+			roleService.save(role);
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
 	}
@@ -193,7 +204,7 @@ public class EagleEyeController {
 
 	@RequestMapping(value = "getRole/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Role> getRole(@PathVariable("id") Long id) {
-		Role role = roleservice.getById(id);
+		Role role = roleService.getById(id);
 		if (role == null) {
 			logger.debug("Role with id " + id + "  does not exists");
 			return new ResponseEntity<Role>(HttpStatus.NOT_FOUND);
@@ -206,7 +217,7 @@ public class EagleEyeController {
 
 	@RequestMapping(value="/getRoles",method = RequestMethod.GET)
 	public ResponseEntity<List<Role>> getAllRoles() {
-		List<Role> roleList = roleservice.getAll();
+		List<Role> roleList = roleService.getAll();
 		if (roleList.isEmpty()) {
 			logger.debug("Role does not exists");
 			return new ResponseEntity<List<Role>>(HttpStatus.NO_CONTENT);
@@ -219,14 +230,58 @@ public class EagleEyeController {
 
 	@RequestMapping(value = "deleteRole/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deleteRole(@PathVariable("id") Long id) {
-		Role role = roleservice.getById(id);
+		Role role = roleService.getById(id);
 		if (role == null) {
 			logger.debug("Role with id " + id + " does not exists");
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 		} else {
-			roleservice.delete(id);
+			roleService.delete(id);
 			logger.debug("Role with id " + id + " deleted");
 			return new ResponseEntity<Void>(HttpStatus.GONE);
 		}
 	}
+	
+	//Ground Controller
+	
+	@RequestMapping(value="/addGround",method = RequestMethod.POST)
+	public ResponseEntity<List<Ground>> addGround(@RequestBody Ground ground) {
+		groundService.save(ground);
+		logger.debug("Added:: " + ground);
+		List<Ground> grounds = groundService.getAll();
+		return new ResponseEntity<List<Ground>>(grounds, HttpStatus.CREATED);
+	}
+	
+	
+	@RequestMapping(value="/getGroundByPlace", method=RequestMethod.POST)
+	public ResponseEntity<List<Ground>> getGroundByPlace(@RequestBody Ground ground){
+		String city=ground.getCity();
+		String place=ground.getPlace();
+		
+		List<Ground> grounds=groundDao.getGroundByCity(city, place);
+		return new ResponseEntity<List<Ground>>(grounds, HttpStatus.OK);
+		
+	}
+	
+	@RequestMapping(value="/bookSlot",method=RequestMethod.GET)
+	public ResponseEntity< List<Ground>> bookSlot(){
+		 List<Ground> objs = new ArrayList() ;
+		
+		objs.addAll(groundDao.getLocationList());   
+		
+		 return new ResponseEntity<List<Ground>>(objs,HttpStatus.OK);
+		
+	}
+	@RequestMapping(value="/getAllGrounds",method=RequestMethod.GET)
+	public ResponseEntity<List<Ground>> getAllGrounds(){
+		List<Ground> groundList=groundService.getAll();
+		if (groundList.isEmpty()) {
+			logger.debug("Ground does not exists");
+			return new ResponseEntity<List<Ground>>(HttpStatus.NO_CONTENT);
+		}
+		logger.debug("Found " + groundList.size() + " Grounds");
+		logger.debug(Arrays.toString(groundList.toArray()));
+		return new ResponseEntity<List<Ground>>(groundList,HttpStatus.OK);
+		
+	}
+	
 }
